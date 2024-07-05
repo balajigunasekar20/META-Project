c441d2635948a7892d225b75fd004117e4234756
Balaji Gunasekar <73152702+balajigunasekar20@users.noreply.github.com>
2024-07-05 16:50:28 +0530

    Add files via upload

---
+++ src/components/BookingForm.js
@@ -0,0 +1,62 @@
+import React from "react";
+import {useState} from 'react';
+ const BookingForm =(props)=>{
+    const [date, setDate] = useState("");
+    const [times, setTimes] = useState("");
+    const [guests, setGuest] = useState("");
+    const [occasion, setOccasion] = useState("");
+
+    const handleSubmit = (e) =>{
+        e.preventDefault();
+        props.submitForm(e);
+    }
+
+    const handleChange = (e) =>{
+        setDate(e);
+        props.dispatch(e);
+    }
+    return(
+        <header>
+            <section>
+                <form onSubmit={handleSubmit}>
+                    <fieldset>
+                        <div>
+                            <label htmlFor='book-date'>Choose Date:</label>
+                            <input  id='book-date' value={date} onChange={(e) => handleChange(e.target.value)} type='date' required />
+                        </div>
+                        <div>
+                            <label htmlFor='book-time'>Choose Time:</label>
+                            <select id='book-time' value={times} onChange={(e)=>setTimes(e.target.value)} required>
+                                <option value=''>Select a Time</option>
+                                {
+                                    props.availableTimes.availableTimes.map(availableTimes => {return <option
+                                    key={availableTimes}>{availableTimes}</option>})
+                                }
+
+                            </select>
+                        </div>
+                        <div>
+                            <label htmlFor="book-guests">Number of guests:</label>
+                            <input id='nook-guests' min='1' value={guests} onChange={(e)=> setGuest(e.target.value)} 
+                            type={"number"} placeholder={0} max={10} required/>
+                        </div>
+
+                        <div>
+                            <label htmlFor="book-occasion">Occasion:</label>
+                            <select id='book-occasion' key={occasion} value={occasion} onChange={e => setOccasion(e.target.value)} required>
+                                <option value="">Select an option</option>
+                                <option>Birthday</option>
+                                <option>Anniversary</option>
+                            </select>
+                        </div>
+                        <div className="btnReceive">
+                            <input aria-label='On Click' type={'submit'} value={"Make Your Reservation"} />
+                        </div>
+                    </fieldset>
+
+                </form>
+            </section>
+        </header>
+    );
+ };
+ export default BookingForm;
